8e8caf31c577d23fd4b12cbe93912a868166c5c3
Ernesto Laval <ernesto.laval@gmail.com>
2015-05-22 11:57:18 +0100

    reduce

--- deps.sh
+++ deps.sh
@@ -1,9 +1,7 @@
-#!/bin/bash
-set -o errexit
-# apt-get update \
-# 	&& apt-get install -y libudev-dev \
-# 	&& apt-get install -y libusb-1.0-0-dev \
-# 	&& apt-get install -y git
-cd my-node-red
-npm install
+ #!/bin/bash
 
+set -o errexit
+
+apt-get -q update
+apt-get install -y cowsay
+/usr/games/cowsay "Install dependencies using a script like this!"
